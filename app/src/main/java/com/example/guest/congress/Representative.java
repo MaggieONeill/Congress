789ab4a64b1ee6b074261c961e9abefb3f6e4580
maggieoneill <margaretanneoneill@gmail.com>
2015-11-09 13:18:14 -0800

    added api stuff

---
+++ app/src/main/java/com/example/guest/congress/Representative.java
@@ -0,0 +1,66 @@
+package com.example.guest.congress;
+
+/**
+ * Created by Guest on 11/9/15.
+ */
+public class Representative {
+
+    private String mName;
+    private String mParty;
+    private String mGender;
+    private String mBirthday;
+    private String mPhone;
+    private String mOffice;
+
+    public Representative() {
+    }
+
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getParty() {
+        return mParty;
+    }
+
+    public void setParty(String party) {
+        mParty = party;
+    }
+
+    public String getGender() {
+        return mGender;
+    }
+
+    public void setGender(String gender) {
+        mGender = gender;
+    }
+
+    public String getBirthday() {
+        return mBirthday;
+    }
+
+    public void setBirthday(String birthday) {
+        mBirthday = birthday;
+    }
+
+    public String getPhone() {
+        return mPhone;
+    }
+
+    public void setPhone(String phone) {
+        mPhone = phone;
+    }
+
+    public String getOffice() {
+        return mOffice;
+    }
+
+    public void setOffice(String office) {
+        mOffice = office;
+    }
+}
